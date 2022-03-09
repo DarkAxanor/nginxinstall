903aba6317ceba4cdbf79aecc6c6109d144d1fa1
DarkAxanor <64281987+DarkAxanor@users.noreply.github.com>
2022-03-09 12:44:05 -0600

    Update nginx.sh
    commented lines for update and install

--- nginx.sh
+++ nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update apt cache.
-sudo apt-get update -y
+#sudo apt-get update -y
 
 # Install Nginx.
-sudo apt-get install -y nginx
+#sudo apt-get install -y nginx
 
 # Set the home page.
 echo "<html><body><h2>Welcome! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
